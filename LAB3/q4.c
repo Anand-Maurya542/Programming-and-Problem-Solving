32c75196560ba17a9d135e3e6843d140d2c3f5b5
Anand Maurya <104004354+Anand-Maurya542@users.noreply.github.com>
2025-09-08 16:31:03 +0530

    Add files via upload

---
+++ LAB3/q4.c
@@ -0,0 +1,31 @@
+/* C program that accepts (from the keyboard) a positive integer less than 1000
+and prints out the sum of the digits of this number. For example if the integer is 245,
+then the program should print 11. The program must also print an error message if
+the input is outside the range.
+
+Written By - Anand Kumar Maurya , 2025CA014
+Date - 08/09/2025
+
+*/
+
+#include <stdio.h>
+int main(){
+	int num,sum=0;
+	printf("Enter number : ");
+	scanf("%d",&num);
+	if(num<0 || num>=1000){
+		printf("Error , give number less than 1000.");}
+	else {
+		while(num>=0){
+			sum=num%10;
+			num=num/10;
+		
+		}
+
+	}
+
+printf("Sum of digits is : %d",sum);
+	
+	
+	return 0;
+}
